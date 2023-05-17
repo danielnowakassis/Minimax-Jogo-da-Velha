f90be806a79167e0571ed6a51a209836db466544
danielnowakassis <daniel.nowak.assis@gmail.com>
2023-05-17 16:30:26 -0300

    minimax totalmente alterado
    Agora o jogo tem algum "raciocinio" para ganhar. Pelo menos quando o max_depth = 5 ele não evita derrotas.

--- Board.py
+++ Board.py
@@ -15,14 +15,16 @@ def timer(func):
         t2 = round(time.perf_counter() - t1, 6)
         print("Tempo da jogada: " + str(t2))
         return res
+
     return wrapper
+
 
 class Board:
     def __init__(self):
-        self.board = np.zeros(shape = (4,4))
+        self.board = np.zeros(shape=(4, 4))
         self.possible_plays = [a for a in range(16)]
         self.plays_ocurred = 0
-    
+
     def render(self):
         """
         Plota o tabuleiro
@@ -52,54 +54,65 @@ class Board:
             system('cls')
         else:
             system('clear')
-    
-    def check_win(self, jogada):
+
+    def check_win(self):
         """
         verifica se algum jogador ganhou a partida, dado sua jogada
         """
-        if (jogada-1) // 4 in [0,3] and (jogada-1) % 4 in [0, 3]:  # Verifica diagonal para os cantos
+        for i in range(4):
             possible = 0
-            if (jogada - 1) % 5 == 0:
-                for i in range(4):
-                    possible += self.board[i][i]       
-            else:
-                for i in range(4):
-                    possible += self.board[i][3 - i]       
+            for j in range(4):
+                possible += jogo.board[i][j]
             if abs(possible) == 4:
                 return True
-        #verfica horizontal
+
+        for i in range(4):
+            possible = 0
+            for j in range(4):
+                possible += jogo.board[j][i]
+            if abs(possible) == 4:
+                return True
+
         possible = 0
         for i in range(4):
-            possible += self.board[(jogada-1) // 4][i]  
+            possible += jogo.board[i][i]
         if abs(possible) == 4:
             return True
-        #verfica vertical
         possible = 0
         for i in range(4):
-            possible += self.board[i][(jogada-1) % 4]
-        return abs(possible) == 4   
+            possible += jogo.board[i][3 - i]
+        if abs(possible) == 4:
+            return True
+
+        return False
 
     def contar_jogada(self):
         self.plays_ocurred += 1
         print(f"Jogada nº {self.plays_ocurred}")
+
+    def count_play(self, jogada : int, index : int):
+        self.possible_plays.remove(jogada)
+        self.board[(jogada) // 4][(jogada) % 4] = index
+        self.contar_jogada()
+
 
 class Jogador:
 
     def __init__(self, index):
         self.id = index
 
-
-    def make_play(self, board : Board):
+    def make_play(self, board: Board):
         """
         método para jogador realizar jogada e alterar estado do tabuleiro
         """
 
+
 class Humano(Jogador):
     def __init__(self):
-        super().__init__(index = 1)
+        super().__init__(index=1)
 
     @timer
-    def make_play(self, tabuleiro : Board):
+    def make_play(self, tabuleiro: Board):
         """
         método para jogador realizar jogada e alterar estado do tabuleiro
         """
@@ -107,16 +120,16 @@ class Humano(Jogador):
         a = -1
         while play_not_made:
             a = int(input("Jogada (numpad) : 1...16 "))
-            if tabuleiro.board[(a-1) // 4][(a-1) % 4] == 0:
-                tabuleiro.board[(a-1) // 4][(a-1) % 4] = self.id
-                tabuleiro.possible_plays.remove(a - 1)
+            if tabuleiro.board[(a - 1) // 4][(a - 1) % 4] == 0:
                 play_not_made = False
-                tabuleiro.contar_jogada()
             else:
-                print("Jogada" + str(a) + "não é possível. Tente outra jogada")
-        return a
+                print("Jogada " + str(a) + " não é possível. Tente outra jogada")
+        return a - 1
 
-POSSIBLE_TYPES = ["Random", "MiniMax", "MiniMaxAlphaBeta" ]
+
+POSSIBLE_TYPES = ["Random", "MiniMax", "MiniMaxAlphaBeta"]
+
+
 class Maquina(Jogador):
     def __init__(self, index, type, max_depth):
         super().__init__(index)
@@ -127,38 +140,64 @@ class Maquina(Jogador):
         if type == "Random":
             self.random = Random()
         else:
-          self.max_depth = max_depth
+            self.max_depth = max_depth
+
     @timer
-    def make_play(self, board : Board):
+    def make_play(self, board: Board):
         """
-        método para maquina realizar jogada e alterar estado do tabuleiro
+        método para maquina realizar jogada
         """
         a = -1
         if self.type == "Random":
-            play = board.possible_plays[self.random.randint(0, len(board.possible_plays)-1)]
-            board.possible_plays.remove(play)
-            board.board[(play) // 4][(play) % 4] = self.id
-            board.contar_jogada()
+            play = board.possible_plays[self.random.randint(0, len(board.possible_plays) - 1)]
             a = play
-            
         elif self.type == "MiniMax":
             copia = copy.deepcopy(board)
-            play = self.minimax(copia, 1, -self.id)[0]
-            assert play in range(16)
-            board.possible_plays.remove(play)
-            board.board[(play) // 4][(play) % 4] = self.id
-            board.contar_jogada()
+            minimax = self.minimax(copia, 1, self.id)
+            print(minimax)
+            print()
+            play = minimax[0]
             a = play
         else:
             "TODO"
-        
+        assert a in range(16)
         return a
 
-    def eval(self, jogo, player):
-        """
-        Heuristica de avaliação.
-        
-        """
+    def win_state(self,jogo, player):
+        for i in range(4):
+            possible = 0
+            for j in range(4):
+                possible += jogo.board[i][j]
+            if possible == player * 4:
+                return 1
+            elif possible == -player * 4:
+                return -1
+        for i in range(4):
+            possible = 0
+            for j in range(4):
+                possible += jogo.board[j][i]
+            if possible == player * 4:
+                return 1
+            elif possible == -player * 4:
+                return -1
+
+        possible = 0
+        for i in range(4):
+            possible += jogo.board[i][i]
+        if possible == player * 4:
+            return 1
+        elif possible == -player * 4:
+            return -1
+        possible = 0
+        for i in range(4):
+            possible += jogo.board[i][3 - i]
+        if possible == player * 4:
+            return 1
+        elif possible == -player * 4:
+            return -1
+        return 0.0
+
+    def possible_wins(self, jogo, player):
         n = 0
         for i in range(4):
             possible = 0
@@ -167,7 +206,7 @@ class Maquina(Jogador):
                     possible += 1
             if possible == 4:
                 n += 1
-        
+
         for i in range(4):
             possible = 0
             for j in range(4):
@@ -183,7 +222,7 @@ class Maquina(Jogador):
         if possible == 4:
             n += 1
         possible = 0
-  
+
         for i in range(4):
             if jogo.board[i][3 - i] == 0 or jogo.board[i][3 - i] == player:
                 possible += 1
@@ -192,76 +231,82 @@ class Maquina(Jogador):
 
         return n
 
-    def minimax(self,state, depth, player, jogada = (-1, np.inf)):
+    def eval(self, jogo, player):
         """
-        
+        Heuristica de avaliação.
+
         """
-        if depth % 2 == 1: # depth ímpar
-          agent = 'MAX'
+        #return self.possible_wins(jogo, 1) - self.possible_wins(jogo, -1)
+        return self.win_state(jogo, player)
+
+    def minimax(self, state, depth, player, jogada=(-1, np.inf)):
+        """
+
+        """
+        if depth % 2 == 1:  # depth ímpar
+            agent = 'MAX'
         else:
-          agent = 'MIN' # depth par
+            agent = 'MIN'  # depth par
         if len(state.possible_plays) == 1 or depth == self.max_depth:
-          if agent == 'MAX':
-            max_v = (-1, -np.inf)
-            for cell in state.possible_plays:
-              copia = copy.deepcopy(jogo)
-              copia.board[cell // 4][cell % 4] = player 
-              copia.possible_plays.remove(cell)
-              heuristica = self.eval(copia, player)
-              if max_v[1] < heuristica:
-                max_v = (cell, heuristica)
-              #print(max_v)
-              return max_v
-          else:
-            min_v = (-1, +np.inf)
-            for cell in jogo.possible_plays:
-              copia = copy.deepcopy(jogo)
-              copia.board[(cell) // 4][(cell) % 4] = player 
-              copia.possible_plays.remove(cell)
-              heuristica = self.eval(copia, player)
-              if min_v[1] > heuristica:
-                min_v = (cell, heuristica)
-              #print(min_v)
-              return min_v
+            if agent == 'MAX':
+                max_v = (-1, -np.inf)
+                for cell in state.possible_plays:
+                    copia = copy.deepcopy(jogo)
+                    copia.board[cell // 4][cell % 4] = player
+                    copia.possible_plays.remove(cell)
+                    heuristica = self.eval(copia, player)
+                    if max_v[1] < heuristica:
+                        max_v = (cell, heuristica, copia)
+                    # print(max_v)
+                    return max_v
+            else:
+                min_v = (-1, +np.inf)
+                for cell in jogo.possible_plays:
+                    copia = copy.deepcopy(jogo)
+                    copia.board[(cell) // 4][(cell) % 4] = player
+                    copia.possible_plays.remove(cell)
+                    heuristica = self.eval(copia, player)
+                    if min_v[1] > heuristica:
+                        min_v = (cell, heuristica, copia)
+                    # print(min_v)
+                    return min_v
         elif agent == 'MAX':
-          return self.max_value(state, depth,player, jogada)
+            return self.max_value(state, depth, player, jogada)
         elif agent == 'MIN':
-          return self.min_value(state, depth,-player, jogada)
-      
-    def max_value(self, jogo : Board, depth, player, jogada):
-      max_v = (-1, -np.inf)
-      for cell in jogo.possible_plays:
-        copia = copy.deepcopy(jogo)
-        copia.board[(cell) // 4][(cell) % 4] = player 
-        copia.possible_plays.remove(cell)
-        heuristica = self.eval(copia, player)
-        max_v = self.maximo(max_v, self.minimax(copia, depth + 1, player, jogada))
-      return max_v
-      
-        
-    def min_value(self, jogo : Board, depth, player, jogada):
-      min_v = (-1, +np.inf)
-      for cell in jogo.possible_plays:
-        copia = copy.deepcopy(jogo)
-        copia.board[(cell) // 4][(cell) % 4] = player 
-        copia.possible_plays.remove(cell)
-        heuristica = self.eval(copia, player)
-        min_v = self.minimo((cell, heuristica), self.minimax(copia, depth + 1, player, jogada))
-      return min_v
+            return self.min_value(state, depth, player, jogada)
+
+    def max_value(self, jogo: Board, depth, player, jogada):
+        max_v = (-1, -np.inf, 0)
+        for cell in jogo.possible_plays:
+            copia = copy.deepcopy(jogo)
+            copia.board[(cell) // 4][(cell) % 4] = player
+            copia.possible_plays.remove(cell)
+            heuristica = self.eval(copia, player)
+            max_v = self.maximo((cell, heuristica,copia), self.minimax(copia, depth + 1, player, jogada))
+        return max_v
+
+    def min_value(self, jogo: Board, depth, player, jogada):
+        min_v = (-1, +np.inf, 0)
+        for cell in jogo.possible_plays:
+            copia = copy.deepcopy(jogo)
+            copia.board[(cell) // 4][(cell) % 4] = player
+            copia.possible_plays.remove(cell)
+            heuristica = self.eval(copia, player)
+            min_v = self.minimo((cell, heuristica, copia), self.minimax(copia, depth + 1, player, jogada))
+        return min_v
 
     def maximo(self, tuple_v, heuristic):
-      if tuple_v[1] < heuristic[1]:
-        return heuristic
-      else:
-        return tuple_v
+        if tuple_v[1] < heuristic[1]:
+            return heuristic
+        else:
+            return tuple_v
 
     def minimo(self, tuple_v, heuristic):
-      if tuple_v[1] > heuristic[1]:
-        return heuristic
-      else:
-        return tuple_v
+        if tuple_v[1] > heuristic[1]:
+            return heuristic
+        else:
+            return tuple_v
 
-    
 
 if __name__ == "__main__":
     """
@@ -270,26 +315,26 @@ if __name__ == "__main__":
     inteligenca_bots = "MiniMax"
     jogo = Board()
     n_jogadas = 0
-    maquina = Maquina(-1, inteligenca_bots, 5)
+    maquina = Maquina(-1, inteligenca_bots, max_depth=5)
     modo = ""
 
     """
     #Seleção do modo de jogo
     """
     print("Selecione o modo de jogo:")
-    print("-Jogador Versus Máquina (JxM)")
-    print("-Máquina Versus Máquina (MxM)")
+    print("-Jogador Versus Máquina (1)")
+    print("-Máquina Versus Máquina (2)")
 
     while True:
         modo = input()
-        if modo in ["JxM", "MxM"]:
+        if modo in ["1", "2"]:
             break
         raise Exception("Selecione um modo válido")
 
-    if modo == "JxM":
+    if modo == "1":
         jogador = Humano()
     else:
-        jogador = Maquina(1, inteligenca_bots, 5)
+        jogador = Maquina(1, inteligenca_bots, max_depth=5)
 
     """
     Início do jogo
@@ -298,22 +343,24 @@ if __name__ == "__main__":
         print()
         jogo.render()
         print()
-        #if modo == "MxM":
+        # if modo == "MxM":
         #    time.sleep(1)
         jogada = jogador.make_play(jogo)
-        if jogo.check_win(jogada):
+        jogo.count_play(jogada, 1)
+        if jogo.check_win():
             print("JOGADOR 1 GANHOU")
             break
-        jogo.clean()
+        # jogo.clean()
 
         jogo.render()
         print()
         jogada = maquina.make_play(jogo)
-        if jogo.check_win(jogada):
+        jogo.count_play(jogada, -1)
+        if jogo.check_win():
             print("JOGADOR 2 GANHOU")
             break
         elif jogo.plays_ocurred == 16:
             print("EMPATE")
             break
-        #jogo.clean()
+        # jogo.clean()
     jogo.render()
